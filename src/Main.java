281c06b77dd2fd8c1ad82db1423fa03cea9d08b6
IgorOg <tigerretyo@mail.ru>
2023-03-23 18:54:02 +0300

    class Main is change

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,15 @@ public class Main {
    static Integer in = 2;
    static Float fl = 2.6f;
    static Short sh;
+
     static Double D = 4.5;
    static String st = "27";
    public static void main(String[] args){
+       int number = 324;
+       int age = 32;
        Number y = D;
        Integer r = y.intValue();
        System.out.println(r);
    }
+
 }
